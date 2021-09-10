5a3cf685de2f2eb5c44dbcf35dadf7b386421935
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-09-10 09:53:38 +0530

    chore: added puppeteer steam

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = process.env.SERVER_PORT;
+const { launch, getStream } = require("puppeteer-stream");
+const fs = require("fs");
+const path = require('path');
+const dir = path.join(__dirname, "..", "data");
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const file = fs.createWriteStream(path.join(dir, "test.webm"));
+async function record() {
+    const browser = await launch({
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+    });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    const page = await browser.newPage();
+    await page.goto("https://www.youtube.com/watch?v=_kUrW9SEaJc");
+    const stream = await getStream(page, { audio: true, video: true });
+    console.log("recording");
+
+    stream.pipe(file);
+    setTimeout(async () => {
+        await stream.destroy();
+        file.close();
+        console.log("finished");
+    }, 1000 * 10);
+}
+
+record();
